f2ba288b206b78353e5e7b3bd44d3490b517203a
Eduard Hakobyan <sargisabrahamyan4@gmail.com>
2024-04-22 12:03:19 +0400

    reset main js

--- functions/enqueue.php
+++ functions/enqueue.php
@@ -25,44 +25,44 @@ function enqueue_assets()
 	// wp_enqueue_script("bootstrap-5", "//cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js", "", "", true);
 	// wp_script_add_data( 'bootstrap-5', array( 'integrity', 'crossorigin' ) , array( 'sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM', 'anonymous' ) );
 
-	// wp_enqueue_script("jquery", get_template_directory_uri() . "/js/jQuery-v-3-4-1.js", "", "", true);
+	wp_enqueue_script("jquery", get_template_directory_uri() . "/js/jQuery-v-3-4-1.js", "", "", true);
 	wp_enqueue_script("swiper", get_template_directory_uri() . "/js/swiper-v-8-3-1.js", "", "", true);
 	wp_enqueue_script("telephone", get_template_directory_uri() . "/js/telephone-input-v-17-0-13.js", "", "", true);
 	wp_enqueue_script("data-aos", get_template_directory_uri() . "/js/data-aos.js", "", "", true);
 	wp_enqueue_script("main-js", get_template_directory_uri() . "/js/main.js", "", "", true);
-	// if (is_front_page()) {
-	// 	wp_enqueue_script("home-js", get_template_directory_uri() . "/js/page/home.js", "", "", true);
-	// }
-	// if (is_page()) {
-	// 	$page_template = get_page_template_slug(get_the_ID());
-	// 	if ($page_template == 'page-certificates.php') {
-	// 		wp_enqueue_script("cert-js", get_template_directory_uri() . "/js/page/certificate.js", "", "", true);
-	// 	}
-	// 	if ($page_template == 'page-services.php') {
-	// 		wp_enqueue_script("service-js", get_template_directory_uri() . "/js/page/service.js", "", "", true);
-	// 	}
-	// 	if ($page_template == 'page-line.php') {
-	// 		wp_enqueue_script("line-js", get_template_directory_uri() . "/js/page/single-service-aircraft.js", "", "", true);
-	// 	}
-	// 	if ($page_template == 'page-aog.php') {
-	// 		wp_enqueue_script("aog-js", get_template_directory_uri() . "/js/page/single-service-aog-maintenance.js", "", "", true);
-	// 	}
-	// 	if ($page_template == 'page-battery.php') {
-	// 		wp_enqueue_script("battery-js", get_template_directory_uri() . "/js/page/single-service-aircraft-battery.js", "", "", true);
-	// 	}
-	// 	if ($page_template == 'page-wheel.php') {
-	// 		wp_enqueue_script("wheel-js", get_template_directory_uri() . "/js/page/single-service-aircraft-wheel.js", "", "", true);
-	// 	}
-	// 	if ($page_template == 'page-detailing.php') {
-	// 		wp_enqueue_script("detailing-js", get_template_directory_uri() . "/js/page/single-service-aircraft-detailing.js", "", "", true);
-	// 	}
-	// 	if ($page_template == 'page-borescope.php') {
-	// 		wp_enqueue_script("borescope-js", get_template_directory_uri() . "/js/page/single-service-borescope.js", "", "", true);
-	// 	}
-	// 	if ($page_template == 'page-ndt.php') {
-	// 		wp_enqueue_script("ndt-js", get_template_directory_uri() . "/js/page/single-service-aircraft-ndt.js", "", "", true);
-	// 	}
-	// }
+	if (is_front_page()) {
+		wp_enqueue_script("home-js", get_template_directory_uri() . "/js/page/home.js", "", "", true);
+	}
+	if (is_page()) {
+		$page_template = get_page_template_slug(get_the_ID());
+		if ($page_template == 'page-certificates.php') {
+			wp_enqueue_script("cert-js", get_template_directory_uri() . "/js/page/certificate.js", "", "", true);
+		}
+		if ($page_template == 'page-services.php') {
+			wp_enqueue_script("service-js", get_template_directory_uri() . "/js/page/service.js", "", "", true);
+		}
+		if ($page_template == 'page-line.php') {
+			wp_enqueue_script("line-js", get_template_directory_uri() . "/js/page/single-service-aircraft.js", "", "", true);
+		}
+		if ($page_template == 'page-aog.php') {
+			wp_enqueue_script("aog-js", get_template_directory_uri() . "/js/page/single-service-aog-maintenance.js", "", "", true);
+		}
+		if ($page_template == 'page-battery.php') {
+			wp_enqueue_script("battery-js", get_template_directory_uri() . "/js/page/single-service-aircraft-battery.js", "", "", true);
+		}
+		if ($page_template == 'page-wheel.php') {
+			wp_enqueue_script("wheel-js", get_template_directory_uri() . "/js/page/single-service-aircraft-wheel.js", "", "", true);
+		}
+		if ($page_template == 'page-detailing.php') {
+			wp_enqueue_script("detailing-js", get_template_directory_uri() . "/js/page/single-service-aircraft-detailing.js", "", "", true);
+		}
+		if ($page_template == 'page-borescope.php') {
+			wp_enqueue_script("borescope-js", get_template_directory_uri() . "/js/page/single-service-borescope.js", "", "", true);
+		}
+		if ($page_template == 'page-ndt.php') {
+			wp_enqueue_script("ndt-js", get_template_directory_uri() . "/js/page/single-service-aircraft-ndt.js", "", "", true);
+		}
+	}
 	wp_localize_script('jquery', 'wp_var', array(
 		'ajax_url' => admin_url('admin-ajax.php')
 	));
