4643957f7f4ceff944bcaaa1ffc8f0a80d4fa50e
Eduard Hakobyan <sargisabrahamyan4@gmail.com>
2024-05-10 16:31:10 +0400

    new change

--- front-page.php
+++ front-page.php
@@ -20,29 +20,24 @@
 
 
 				<?php
-				if (have_rows('home_our_service')):
-					while (have_rows('home_our_service')):
-						the_row();
-						$title = get_sub_field('home_our_service_title');
-						$image = get_sub_field('home_our_service_image');
-						$link = get_sub_field('home_our_service_link');
-						?>
+				$title = get_sub_field('home_our_service_title');
+				$image = get_sub_field('home_our_service_image');
+				$link = get_sub_field('home_our_service_link');
+				?>
 
-						<li class="services-content-list" data-aos="fade-right">
-							<img class="services-content-image" src="<?= $image ?>" alt="">
-							<a class="services-content-link" href="<?= $link ?>">
-								<span class="service-ttile"><?= $title ?></span>
-								<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
-									<path class="card-icon-stroke"
-										d="M25.0003 25.0001V15.0001M25.0003 15.0001H15.0003M25.0003 15.0001L15.0003 24.9999M13 35H27C29.8003 35 31.2004 35 32.27 34.455C33.2108 33.9757 33.9757 33.2108 34.455 32.27C35 31.2004 35 29.8003 35 27V13C35 10.1997 35 8.79961 34.455 7.73005C33.9757 6.78924 33.2108 6.02433 32.27 5.54497C31.2004 5 29.8003 5 27 5H13C10.1997 5 8.79961 5 7.73005 5.54497C6.78924 6.02433 6.02433 6.78924 5.54497 7.73005C5 8.79961 5 10.1997 5 13V27C5 29.8003 5 31.2004 5.54497 32.27C6.02433 33.2108 6.78924 33.9757 7.73005 34.455C8.79961 35 10.1997 35 13 35Z"
-										stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-								</svg>
-							</a>
-						</li>
+				<li class="services-content-list" data-aos="fade-right">
+					<img class="services-content-image" src="<?= $image ?>" alt="">
+					<a class="services-content-link" href="<?= $link ?>">
+						<span class="service-ttile"><?= $title ?></span>
+						<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
+							<path class="card-icon-stroke"
+								d="M25.0003 25.0001V15.0001M25.0003 15.0001H15.0003M25.0003 15.0001L15.0003 24.9999M13 35H27C29.8003 35 31.2004 35 32.27 34.455C33.2108 33.9757 33.9757 33.2108 34.455 32.27C35 31.2004 35 29.8003 35 27V13C35 10.1997 35 8.79961 34.455 7.73005C33.9757 6.78924 33.2108 6.02433 32.27 5.54497C31.2004 5 29.8003 5 27 5H13C10.1997 5 8.79961 5 7.73005 5.54497C6.78924 6.02433 6.02433 6.78924 5.54497 7.73005C5 8.79961 5 10.1997 5 13V27C5 29.8003 5 31.2004 5.54497 32.27C6.02433 33.2108 6.78924 33.9757 7.73005 34.455C8.79961 35 10.1997 35 13 35Z"
+								stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+						</svg>
+					</a>
+				</li>
 
-						<?Php
-					endwhile;
-				endif; ?>
+		
 
 
 			</ul>
