695bf3905a5af1fb351644bec8a077a103b59b2c
Eduard Hakobyan <sargisabrahamyan4@gmail.com>
2024-05-16 17:01:52 +0400

    a

--- page-aog.php
+++ page-aog.php
@@ -1,12 +1,12 @@
 <?php get_header();
 /* Template Name: AOG Maintenance Template */
 ?>
-<section class="aog-hero">
-    <div class="aog-title-subtitle-inner">
-        <h1 class="cover-title about-cover-title"><?= the_field('title'); ?></h1>
-        <h2 class="subtitle aog-cover-subtitle"><?= the_field('description'); ?></h2>
+<section class="service-cover aircraft-maintenance">
+    <div class="about-title-subtitle-inner">
+        <h1 class="cover-title about-cover-title" data-aos="fade-left"><?= the_field('title'); ?></h1>
+        <h2 class="subtitle about-cover-subtitle" data-aos="fade-left"><?= the_field('description'); ?></h2>
     </div>
-    <img class="aog-single-img" src="<?= the_field('image'); ?>" alt="">
+    <img class="about-cover-image single-service-aircraft-image aog-single-img" src="<?= the_field('image'); ?>" alt="">
 </section>
 <section class="offer-inner aog-inner">
     <p class="title title-before-after-effect none-effect text-left"> <?= the_field('c_heading'); ?></p>
