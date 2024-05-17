ca25d65b524e0704072a847d8eb6bbe6cd057c92
Eduard Hakobyan <sargisabrahamyan4@gmail.com>
2024-05-17 15:32:04 +0400

    a

--- front-page.php
+++ front-page.php
@@ -3,7 +3,9 @@
 <img class="home-linear-2" src="https://x-technics.com/ysteeshy/2024/05/linear.webp" alt="">
 <img class="home-linear-3" src="https://x-technics.com/ysteeshy/2024/05/linear.webp" alt="">
 <section class="home-cover">
-	<img class="home-hero-img" src="<?= the_field('image'); ?>" alt="">
+	<div>
+		<img class="home-hero-img" src="<?= the_field('image'); ?>" alt="">
+	</div>
 	<div class="cover-title-subtitle-inner">
 		<div data-aos="fade-right">
 			<h1 class="cover-title home-cover-title"><?= the_field('title'); ?></h1>
