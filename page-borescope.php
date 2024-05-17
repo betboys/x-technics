37b37a731d3f15ad5099be2a5916965dd8374c6f
Eduard Hakobyan <sargisabrahamyan4@gmail.com>
2024-05-17 11:55:17 +0400

    a

--- page-borescope.php
+++ page-borescope.php
@@ -149,7 +149,7 @@
 
     </ul>
 </section>
-<section class="swiper-slide-inner width-center">
+<section class="swiper-slide-inner width-center swiper-slide-inner-effect">
     <h3 class="title title-before-after-effect slide-title-sytle">Explore Other Services:</h3>
     <div class="swiper-container swiper-after" data-aos="fade-right">
         <?php echo get_template_part('templates/related-pages'); ?>
