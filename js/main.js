b643817ff0ccac1c64af60fcfee0e8eaeddf5fc1
Eduard Hakobyan <sargisabrahamyan4@gmail.com>
2024-05-22 18:48:54 +0400

    a

--- js/main.js
+++ js/main.js
@@ -334,6 +334,15 @@ if (document.querySelector('.aircraft-ndt')) {
     let marginLeft = window.getComputedStyle(headerContainer).getPropertyValue('padding-left');
     swiperContainer[1].setAttribute('style', `margin-left: ${marginLeft}`);
   }
+
+  if (window.innerWidth <= 400) {
+    let aodSlide = document.querySelectorAll(".detailing-slides");
+    let windowWidth = window.innerWidth;
+    let newSlideWidth = windowWidth - 30;
+    for (let i = 0; i < aodSlide.length; i++) {
+      aodSlide[i].setAttribute("style", `width: ${newSlideWidth}px`);
+    }
+  }
 }
 
 // Service single page` aircraft wheel
