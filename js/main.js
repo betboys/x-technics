7c4881d1db7fedef17beea3203253c79ff4502e8
Eduard Hakobyan <sargisabrahamyan4@gmail.com>
2024-05-24 13:20:26 +0400

    a

--- js/main.js
+++ js/main.js
@@ -136,17 +136,17 @@ if (document.querySelector('.home-cover')) {
   const swiper = new Swiper('.swiper', {
     slidesPerView: 'auto',
     speed: 1000,
-    // centeredSlides: true,
+    centeredSlides: true,
     loop: true,
     spaceBetween: 118,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-    // pagination: {
-    //   el: '.swiper-pagination',
-    //   clickable: true,
-    // },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
   });
 }
 
