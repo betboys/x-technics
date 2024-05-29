9806fd00610b047aef2d4ca4c9eebd3ed6895dc3
Eduard Hakobyan <sargisabrahamyan4@gmail.com>
2024-05-29 18:45:54 +0400

    a

--- js/main.js
+++ js/main.js
@@ -148,6 +148,24 @@ if (document.querySelector('.home-cover')) {
       clickable: true,
     },
   });
+
+  if(window.innerWidth <= 744) {
+    const swiper = new Swiper('.swiper', {
+      slidesPerView: 'auto',
+      speed: 1000,
+      centeredSlides: true,
+      loop: true,
+      spaceBetween: 0,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  }
 }
 
 // Service page
