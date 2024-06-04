2f4a1cf7416bbb1b19311c53abf3f9a3fcb1c6c0
Eduard Hakobyan <sargisabrahamyan4@gmail.com>
2024-06-04 17:12:12 +0400

    a

--- templates/contact.php
+++ templates/contact.php
@@ -1,4 +1,4 @@
-<section class="contact-us" data-aos="fade-up">
+<section class="contact-us" id="contact-us" data-aos="fade-up">
 	<h3 class="title title-before-after-effect"><?= the_field('contact_section_title', 'option'); ?></h3>
 	<div class="contact-us-container">
 		<div class="contact-us-socal-mekia-links">
