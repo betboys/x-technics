5611fc6aa73bedc9d94ce696920c69c8d80a451e
Eduard Hakobyan <sargisabrahamyan4@gmail.com>
2024-06-07 11:28:00 +0400

    a

--- page-services.php
+++ page-services.php
@@ -16,29 +16,67 @@
     </div>
 </section>
 <section class="service-provider-container" data-aos="fade-up">
-    <div class="provider-text-inner text-center">
-        <p class="title title-before-after-effect color-style text-center title-center">
-            <?= the_field('s_heading'); ?>
-        </p>
+    <div class="provider-text-inner">
+        <p class="title title-before-after-effect none-effect text-left"><?= the_field('s_heading'); ?></p>
+        <p class="subtitle subtitle-inner-top"><?= the_field('s_desc'); ?></p>
     </div>
 
     <?php
     $featured_posts = get_field('services');
     if ($featured_posts): ?>
-        <ul class="about-cards-container service-card">
+        <ul class="about-cards-container about-main">
             <?php foreach ($featured_posts as $featured_post):
                 $permalink = get_permalink($featured_post->ID);
                 $title = get_the_title($featured_post->ID);
+                $card_subtitle = get_field('card_subtitle', $featured_post->ID);
                 $featured_image_url = get_the_post_thumbnail_url($featured_post->ID, 'full');
                 ?>
                 <li class="about-card-list">
-                    <a class="our-service-card-link" href="<?php echo esc_url($permalink); ?>">
-                        <img class="about-card-img" src="<?php echo $featured_image_url; ?>" alt="">
-                        <p class="service-card-text"> <?php echo esc_html($title); ?></p>
+                    <img class="about-card-img" src="<?php echo $featured_image_url; ?>" alt="">
+                    <p class="about-card-text"> <?php echo esc_html($title); ?></p>
+                    <p class="subtitle about-card-text-hidden"><?php echo $card_subtitle; ?></p>
+                    <a class="about-card-link" href="<?php echo esc_url($permalink); ?>">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <g clip-path="url(#clip0_4019_3743)">
+                                <path
+                                    d="M19.1213 18.3936L23.0403 14.4746C23.6767 13.8078 24.0317 12.9214 24.0317 11.9996C24.0317 11.0779 23.6767 10.1915 23.0403 9.52464L19.1213 5.60564C18.84 5.32425 18.4585 5.16611 18.0607 5.16602C17.6628 5.16592 17.2812 5.32388 16.9998 5.60514C16.7184 5.88641 16.5603 6.26793 16.5602 6.66579C16.5601 7.06365 16.718 7.44525 16.9993 7.72664L19.7793 10.5076L1.5293 10.5306C1.13147 10.5306 0.749941 10.6887 0.468637 10.97C0.187332 11.2513 0.0292969 11.6328 0.0292969 12.0306C0.0292969 12.4285 0.187332 12.81 0.468637 13.0913C0.749941 13.3726 1.13147 13.5306 1.5293 13.5306L19.7603 13.5076L16.9993 16.2726C16.7261 16.5556 16.5749 16.9345 16.5783 17.3278C16.5817 17.721 16.7395 18.0973 17.0176 18.3754C17.2957 18.6535 17.6719 18.8112 18.0652 18.8147C18.4585 18.8181 18.8374 18.6669 19.1203 18.3936H19.1213Z"
+                                    fill="white"></path>
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_4019_3743">
+                                    <rect width="24" height="24" fill="white"></rect>
+                                </clipPath>
+                            </defs>
+                        </svg>
                     </a>
                 </li>
             <?php endforeach;
             wp_reset_postdata(); ?>
+            <?php if (get_field('service_link')): ?>
+                <li class="about-card-list about-card-list-last">
+                    <img class="about-card-img" src="https://x-technics.com/ysteeshy/2024/05/Subtract-style-1.webp" alt="">
+                    <div class="text-link">
+                        <p class="card-text-link">Explore more
+                            by clicking on the button</p>
+                        <a class="swiper-button-next" href="<?= the_field('service_link'); ?>">
+                            <img class="next-st" src="https://x-technics.com/ysteeshy/2024/05/next-st.webp" alt="">
+                            <img class="next-ho" src="https://x-technics.com/ysteeshy/2024/05/next-ho.webp" alt="">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <g clip-path="url(#clip0_4019_3743)">
+                                    <path
+                                        d="M19.1213 18.3936L23.0403 14.4746C23.6767 13.8078 24.0317 12.9214 24.0317 11.9996C24.0317 11.0779 23.6767 10.1915 23.0403 9.52464L19.1213 5.60564C18.84 5.32425 18.4585 5.16611 18.0607 5.16602C17.6628 5.16592 17.2812 5.32388 16.9998 5.60514C16.7184 5.88641 16.5603 6.26793 16.5602 6.66579C16.5601 7.06365 16.718 7.44525 16.9993 7.72664L19.7793 10.5076L1.5293 10.5306C1.13147 10.5306 0.749941 10.6887 0.468637 10.97C0.187332 11.2513 0.0292969 11.6328 0.0292969 12.0306C0.0292969 12.4285 0.187332 12.81 0.468637 13.0913C0.749941 13.3726 1.13147 13.5306 1.5293 13.5306L19.7603 13.5076L16.9993 16.2726C16.7261 16.5556 16.5749 16.9345 16.5783 17.3278C16.5817 17.721 16.7395 18.0973 17.0176 18.3754C17.2957 18.6535 17.6719 18.8112 18.0652 18.8147C18.4585 18.8181 18.8374 18.6669 19.1203 18.3936H19.1213Z"
+                                        fill="white"></path>
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_4019_3743">
+                                        <rect width="24" height="24" fill="white"></rect>
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                        </a>
+                    </div>
+                </li>
+            <?php endif; ?>
         </ul>
     <?php endif; ?>
 </section>
