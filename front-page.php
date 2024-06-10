fe5fd37ba5e022a46c0ed914af95f41a5de4618b
Eduard Hakobyan <sargisabrahamyan4@gmail.com>
2024-06-11 00:08:00 +0400

    a

--- front-page.php
+++ front-page.php
@@ -256,6 +256,7 @@
 				<img class="prev-st" src="https://x-technics.com/ysteeshy/2024/05/prev-st.webp" alt="">
 				<img class="prev-ho" src="https://x-technics.com/ysteeshy/2024/05/prev-ho.webp" alt="">
 				<img class="prev-mobile" src="https://x-technics.com/ysteeshy/2024/05/prev-mobile.webp" alt="">
+				<img class="prev-mobile-ho" src="https://x-technics.com/ysteeshy/2024/06/mobile-ho-btn-prev.png" alt="">
 				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
 					<g clip-path="url(#clip0_4019_3733)">
 						<path
@@ -273,6 +274,7 @@
 				<img class="next-st" src="https://x-technics.com/ysteeshy/2024/05/next-st.webp" alt="">
 				<img class="next-ho" src="https://x-technics.com/ysteeshy/2024/05/next-ho.webp" alt="">
 				<img class="next-mobile" src="https://x-technics.com/ysteeshy/2024/05/next-mobile.webp" alt="">
+				<img class="next-mobile-ho" src="https://x-technics.com/ysteeshy/2024/06/mobile-ho-btn-next.png" alt="">
 				<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
 					<g clip-path="url(#clip0_4019_3743)">
 						<path
