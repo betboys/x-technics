16dfc7b8ae68d0e284a1d8aa175d2efb8938188c
Eduard Hakobyan <sargisabrahamyan4@gmail.com>
2024-06-11 15:24:17 +0400

    a

--- page-services.php
+++ page-services.php
@@ -107,7 +107,7 @@ if (have_rows('services_boxes')):
                     <a class="our-aircraft-link link-left-inner" href="<?= $service_link ?>">
                         <img class="button-standart" src="https://x-technics.com/ysteeshy/2024/05/Button-st.webp" alt="">
                         <img class="button-hover" src="https://x-technics.com/ysteeshy/2024/05/Button-ho.webp" alt="">
-                        <span class="menu-link index">
+                        <span class="menu-link index ser-span">
                             Explore More
                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
                                 <g clip-path="url(#clip0_4006_464)">
