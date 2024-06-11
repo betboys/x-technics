b3ecacac9c848ad8c769e9138294d97c2cb1c809
Eduard Hakobyan <sargisabrahamyan4@gmail.com>
2024-06-11 18:04:11 +0400

    a

--- js/main.js
+++ js/main.js
@@ -13,6 +13,15 @@ function fadeout() {
 if (window.innerWidth <= 744) {
   let aaa = document.querySelectorAll(".swiper-button-next");
   let maaa = document.querySelectorAll(".next-mobile-ho");
+  let a0 = document.querySelector(".cover-link");
+  let a1 = document.querySelector('.cover-link-ho');
+  a0.addEventListener('click', function () {
+    a1.style.opacity = '1';
+    setTimeout(() => {
+      a1.style.opacity = '0';
+    }, 500);
+  })
+
   for (let i = 0; i < aaa.length; i++) {
     aaa[i].addEventListener("click", function () {
       maaa[i].style.opacity = '1';
