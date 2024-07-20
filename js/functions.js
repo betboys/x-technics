a22a149ed55c619f763d4737b1876724bc77cd60
Eduard <eduard.246604@gmail.com>
2024-07-20 14:00:02 +0400

    a

--- js/functions.js
+++ js/functions.js
@@ -1,104 +1,105 @@
-// var $ = jQuery;
-// $(document).ready(function () {
-//   // $(".send-btn-form-cust").click(function (e) {
-//   //   e.preventDefault();
-//   //   var checkedServices = [];
-//   //   $('input[type="checkbox"]:checked').each(function () {
-//   //     checkedServices.push($(this).next(".label").text());
-//   //   });
-//   //   var formData = {
-//   //     action: "send_email",
-//   //     checkedServices: checkedServices,
-//   //     service: $(".select-text").text(),
-//   //     fullName: $('input[name="fullName"]').val(),
-//   //     email: $('input[type="email"]').val(),
-//   //     phoneNumber:
-//   //       $(".iti__selected-dial-code").text() + $('input[type="number"]').val(),
-//   //     message: $("textarea").val(),
-//   //   };
+var $ = jQuery;
+$(document).ready(function () {
+  // $(".send-btn-form-cust").click(function (e) {
+  //   e.preventDefault();
+  //   var checkedServices = [];
+  //   $('input[type="checkbox"]:checked').each(function () {
+  //     checkedServices.push($(this).next(".label").text());
+  //   });
+  //   var formData = {
+  //     action: "send_email",
+  //     checkedServices: checkedServices,
+  //     service: $(".select-text").text(),
+  //     fullName: $('input[name="fullName"]').val(),
+  //     email: $('input[type="email"]').val(),
+  //     phoneNumber:
+  //       $(".iti__selected-dial-code").text() + $('input[type="number"]').val(),
+  //     message: $("textarea").val(),
+  //   };
 
-//   //   $.ajax({
-//   //     url: wp_var.ajax_url,
-//   //     type: "post",
-//   //     data: formData,
-//   //     success: function (response) {
-//   //       // Handle success response
-//   //       $(".contact-us-container .form-inner").html(
-//   //         "<p class='form-title'>Email sent successfully</p>"
-//   //       );
-//   //     },
-//   //     error: function (error) {
-//   //       $(".contact-us-container .form-inner").html(
-//   //         '<p class="form-title">Error:' + error + "</p>"
-//   //       );
-//   //     },
-//   //   });
-//   // });
-//   $(".send-btn-form-cust").click(function (e) {
-//     e.preventDefault();
+  //   $.ajax({
+  //     url: wp_var.ajax_url,
+  //     type: "post",
+  //     data: formData,
+  //     success: function (response) {
+  //       // Handle success response
+  //       $(".contact-us-container .form-inner").html(
+  //         "<p class='form-title'>Email sent successfully</p>"
+  //       );
+  //     },
+  //     error: function (error) {
+  //       $(".contact-us-container .form-inner").html(
+  //         '<p class="form-title">Error:' + error + "</p>"
+  //       );
+  //     },
+  //   });
+  // });
+  $(".send-btn-form-cust").click(function (e) {
+    e.preventDefault();
 
-//     // Basic client-side validation
-//     var fullName = $('input[name="fullName"]').val();
-//     var email = $('input[type="email"]').val();
-//     var phoneNumber = $('input[type="number"]').val();
-//     var message = $("textarea").val();
-//     // if (!fullName || !email || !phoneNumber || !message) {
-//     //   alert("Please fill out all required fields.");
-//     //   return;
-//     // }
+    // Basic client-side validation
+    var fullName = $('input[name="fullName"]').val();
+    var email = $('input[type="email"]').val();
+    var phoneNumber = $('input[type="number"]').val();
+    var message = $("textarea").val();
+    // if (!fullName || !email || !phoneNumber || !message) {
+    //   alert("Please fill out all required fields.");
+    //   return;
+    // }
 
-//     // Extract dial code from phone input field
-//     var dialCode = $(".iti__selected-dial-code").text().trim();
-//     var phoneNumberWithoutDialCode = phoneNumber.substring(dialCode.length);
+    // Extract dial code from phone input field
+    var dialCode = $(".iti__selected-dial-code").text().trim();
+    var phoneNumberWithoutDialCode = phoneNumber.substring(dialCode.length);
 
-//     // Gather form data
-//     var checkedServices = [];
-//     $('input[type="radio"]:checked').each(function () {
-//       checkedServices.push($(this).next(".label").text());
-//     });
-//     var formData = {
-//       action: "send_email",
-//       checkedServices: checkedServices,
-//       service: $(".select-text").text(),
-//       fullName: fullName,
-//       email: email,
-//       phoneNumber: dialCode + phoneNumberWithoutDialCode,
-//       message: message,
-//     };
+    // Gather form data
+    var checkedServices = [];
+    $('input[type="radio"]:checked').each(function () {
+      checkedServices.push($(this).next(".label").text());
+    });
+    var formData = {
+      action: "send_email",
+      checkedServices: checkedServices,
+      service: $(".select-text").text(),
+      fullName: fullName,
+      email: email,
+      phoneNumber: dialCode + phoneNumberWithoutDialCode,
+      message: message,
+    };
 
-//     // Send form data via AJAX
-//     $.ajax({
-//       url: wp_var.ajax_url,
-//       type: "post",
-//       data: formData,
-//       success: function (response) {
-//         if (response === "success") {
-//           // Handle success response
-//           // $(".contact-us-container .form-inner").html(
-//           //   "<p class='form-title'>Email sent successfully</p>"
-//           // );
-//           $('body').css('overflow', 'hidden');
-//           $('#popup-form').css('display', 'flex');
-//         } else {
-//           // Handle error response
-//           // $(".contact-us-container .form-inner").html(
-//           //   '<p class="form-title">Error: Failed to send email</p>'
-//           // );
-//           $('#popup-form').css('display', 'flex');
-//           $('body').css('overflow', 'hidden');
-//         }
-//       },
-//       error: function (xhr, status, error) {
-//         // Handle AJAX error
-//         // $(".contact-us-container .form-inner").html(
-//         //   '<p class="form-title">Error: ' + error + "</p>"
-//         // );
-//         $('body').css('overflow', 'hidden');
-//         $('#popup-form-error').css('display', 'flex');
-//       },
-//     });
-//   });
-// });
+    // Send form data via AJAX
+    $.ajax({
+      url: wp_var.ajax_url,
+      type: "post",
+      data: formData,
+      success: function (response) {
+        if (response === "success") {
+          // Handle success response
+          // $(".contact-us-container .form-inner").html(
+          //   "<p class='form-title'>Email sent successfully</p>"
+          // );
+          $('#popup-form').css('display', 'flex');
+          $('body').css('overflow', 'hidden');
+        }
+        //  else {
+        //   // Handle error response
+        //   // $(".contact-us-container .form-inner").html(
+        //   //   '<p class="form-title">Error: Failed to send email</p>'
+        //   // );
+        //   $('#popup-form').css('display', 'flex');
+        //   $('body').css('overflow', 'hidden');
+        // }
+      },
+    //   error: function (xhr, status, error) {
+    //     // Handle AJAX error
+    //     // $(".contact-us-container .form-inner").html(
+    //     //   '<p class="form-title">Error: ' + error + "</p>"
+    //     // );
+    //     $('body').css('overflow', 'hidden');
+    //     $('#popup-form-error').css('display', 'flex');
+    //   },
+    });
+  });
+});
 
 // $(document).ready(function ($) {
 //   // Attach a click event handler to the button
