2e7502b6b526e56e3120785e81dc2830c9de6abb
eduard246604 <164142708+eduard246604@users.noreply.github.com>
2024-07-26 10:38:08 +0400

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,39 +1,6 @@
 var $ = jQuery;
 $(document).ready(function () {
-  // $(".send-btn-form-cust").click(function (e) {
-  //   e.preventDefault();
-  //   var checkedServices = [];
-  //   $('input[type="checkbox"]:checked').each(function () {
-  //     checkedServices.push($(this).next(".label").text());
-  //   });
-  //   var formData = {
-  //     action: "send_email",
-  //     checkedServices: checkedServices,
-  //     service: $(".select-text").text(),
-  //     fullName: $('input[name="fullName"]').val(),
-  //     email: $('input[type="email"]').val(),
-  //     phoneNumber:
-  //       $(".iti__selected-dial-code").text() + $('input[type="number"]').val(),
-  //     message: $("textarea").val(),
-  //   };
 
-  //   $.ajax({
-  //     url: wp_var.ajax_url,
-  //     type: "post",
-  //     data: formData,
-  //     success: function (response) {
-  //       // Handle success response
-  //       $(".contact-us-container .form-inner").html(
-  //         "<p class='form-title'>Email sent successfully</p>"
-  //       );
-  //     },
-  //     error: function (error) {
-  //       $(".contact-us-container .form-inner").html(
-  //         '<p class="form-title">Error:' + error + "</p>"
-  //       );
-  //     },
-  //   });
-  // });
   $(".send-btn-form-cust").click(function (e) {
     e.preventDefault();
 
@@ -42,6 +9,18 @@ $(document).ready(function () {
     var email = $('input[type="email"]').val();
     var phoneNumber = $('input[type="number"]').val();
     var message = $("textarea").val();
+    if(!fullName) {
+      $('input[name="fullName"]').addClass("error");
+       return;
+    } else {
+        $('input[name="fullName"]').removeClass("error");
+    }
+      if(!email) {
+      $('input[type="email"]').addClass("error");
+       return;
+    } else {
+        $('input[type="email"]').removeClass("error");
+    }
     if (!fullName || !email || !phoneNumber || !message) {
       alert("Please fill out all required fields.");
       return;
