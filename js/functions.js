6fc84a5238414eaaad77ff29ad2f3c0400886318
Eduard Hakobyan <sargisabrahamyan4@gmail.com>
2024-07-27 18:11:08 +0400

    a

--- js/functions.js
+++ js/functions.js
@@ -3,48 +3,43 @@ $(document).ready(function () {
 
   $(".send-btn-form-cust").click(function (e) {
     e.preventDefault();
-
     // Basic client-side validation
     var fullName = $('input[name="fullName"]').val();
     var email = $('input[type="email"]').val();
     var phoneNumber = $('input[type="number"]').val();
     var message = $("textarea").val();
-    if(!fullName) {
+    if (!fullName) {
       $('input[name="fullName"]').addClass("error");
-    
+
     } else {
-        $('input[name="fullName"]').removeClass("error");
+      $('input[name="fullName"]').removeClass("error");
     }
-      if(!email) {
+    if (!email) {
       $('input[type="email"]').addClass("error");
-    
+
     } else {
-        $('input[type="email"]').removeClass("error");
+      $('input[type="email"]').removeClass("error");
     }
-       if(!phoneNumber) {
+    if (!phoneNumber) {
       $('input[type="number"]').addClass("error");
-          $('.phone-input').addClass("error");
-     
+      $('.phone-input').addClass("error");
+
     } else {
-        $('input[type="number"]').removeClass("error");
-         $('.phone-input').removeClass("error");
+      $('input[type="number"]').removeClass("error");
+      $('.phone-input').removeClass("error");
     }
-     if(!message) {
+    if (!message) {
       $("textarea").addClass("error");
 
     } else {
-       $("textarea").removeClass("error");
+      $("textarea").removeClass("error");
     }
- 
     if (!fullName || !email || !phoneNumber || !message) {
- 
-       return;
-     }
-
+      return;
+    }
     // Extract dial code from phone input field
     var dialCode = $(".iti__selected-dial-code").text().trim();
     var phoneNumberWithoutDialCode = phoneNumber.substring(dialCode.length);
-
     // Gather form data
     var checkedServices = [];
     $('input[type="radio"]:checked').each(function () {
@@ -59,7 +54,6 @@ $(document).ready(function () {
       phoneNumber: dialCode + phoneNumberWithoutDialCode,
       message: message,
     };
-
     // Send form data via AJAX
     $.ajax({
       url: wp_var.ajax_url,
@@ -67,66 +61,37 @@ $(document).ready(function () {
       data: formData,
       success: function (response) {
         if (response === "success") {
-          // Handle success response
-          // $(".contact-us-container .form-inner").html(
-          //   "<p class='form-title'>Email sent successfully</p>"
-          // );
           $('body').css('overflow', 'hidden');
           $('#popup-form').css('display', 'flex');
         } else {
-          // Handle error response
-          // $(".contact-us-container .form-inner").html(
-          //   '<p class="form-title">Error: Failed to send email</p>'
-          // );
           $('#popup-form').css('display', 'flex');
           $('body').css('overflow', 'hidden');
         }
       },
       error: function (xhr, status, error) {
-        // Handle AJAX error
-        // $(".contact-us-container .form-inner").html(
-        //   '<p class="form-title">Error: ' + error + "</p>"
-        // );
         $('body').css('overflow', 'hidden');
         $('#popup-form-error').css('display', 'flex');
       },
     });
   });
 });
-
 $(document).ready(function ($) {
-  // Attach a click event handler to the button
   $('.popup-btn').click(function () {
-
-
-    // $('.preloader').css('display', 'flex');
-    // window.setTimeout('fadeout();', 2000);
-    // function fadeout() {
-    //   $('.preloader').delay(2000).fadeOut('slow', function () {
-    //     $('.notLoaded').removeClass('notLoaded');
-    //   });
-    // }
-
-
-    // Your code to execute when the button is clicked
-    // $("html, body").animate({ scrollTop: "0" }, 0);
-    // $('.popup-form').css('display', 'none');
-    // $('body').css('overflow', 'scroll');
-    // You can perform other actions here
-    // For example, make an AJAX request, show/hide elements, or manipulate the DOM
+    $('.preloader').css('display', 'flex');
+    window.setTimeout('fadeout();', 2000);
+    function fadeout() {
+      $('.preloader').delay(2000).fadeOut('slow', function () {
+        $('.notLoaded').removeClass('notLoaded');
+      });
+    }
+    $("html, body").animate({ scrollTop: "0" }, 0);
+    $('.popup-form').css('display', 'none');
+    $('body').css('overflow', 'scroll');
   });
 });
-
-
-
-
 $(document).ready(function ($) {
-  // Attach a click event handler to the button
   $('.popup-btn-error').click(function () {
-
     $('.popup-form-error').css('display', 'none');
-    // $('body').css('overflow', 'scroll');
-    // You can perform other actions here
-    // For example, make an AJAX request, show/hide elements, or manipulate the DOM
+    $('body').css('overflow', 'scroll');
   });
 });
